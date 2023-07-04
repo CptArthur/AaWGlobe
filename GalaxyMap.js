addb2a833d08bf773025f6d3f65041765ef50ebb
CptArthur <59366211+CptArthur@users.noreply.github.com>
2023-07-04 10:46:20 +0200

    Improvements

--- GalaxyMap.js
+++ GalaxyMap.js
@@ -32,11 +32,9 @@ bylenCircle.attr({
 var bylenOuterRing = s.circle(0, 0, 140); // Adjust the size of the outer ring
 bylenOuterRing.attr({
   fill: "none",
-  stroke: "#000000",
+  stroke: "#BCC1B4",
   strokeWidth: 2
 });
-
-
 
 // Click event listener on the center circle (Bylen)
 bylenCircle.click(function () {
@@ -82,7 +80,7 @@ theMoonCircle.click(function () {
 // Create Lezuno planet circle
 var lezunoCircle = s.circle(-100, 100, 40); // Adjust the position and size
 lezunoCircle.attr({
-  fill: "#7F3300",
+  fill: "#CC6322",
   cursor: "pointer"
 });
 
@@ -98,7 +96,7 @@ lezunoCircle.click(function () {
 // Create Lezuno's Moon planet circle
 var lezunoMoonCircle = s.circle(-150, 100, 25); // Adjust the position and size
 lezunoMoonCircle.attr({
-  fill: "#3B3D38",
+  fill: "#99967D",
   cursor: "pointer"
 });
 
@@ -143,6 +141,22 @@ craitCircle.click(function () {
   document.getElementById("crait-info").style.display = "block";
 });
 
+// Create Agaris planet circle
+var S27Diamond = s.ellipse(200, 100, 12,24); // Adjust the position and size
+S27Diamond.attr({
+  fill: "#7F593F",
+  cursor: "pointer"
+});
+
+// Click event listener on Agaris planet circle
+S27Diamond.click(function () {
+  // Hide all planet info menus
+  hideAllPlanetInfoMenus();
+
+  // Show the planet info menu for Agaris
+  document.getElementById("S27-info").style.display = "block";
+});
+
 // Function to hide all planet info menus
 function hideAllPlanetInfoMenus() {
   var planetInfoMenus = document.getElementsByClassName("planet-info");
@@ -156,44 +170,102 @@ function updateCirclePositions() {
     var centerX = window.innerWidth / 2;
     var centerY = window.innerHeight / 2;
 
-  // Update Bylen circle position and outer ring
-  updateCirclePosition(bylenCircle, bylenOuterRing, centerX, centerY);
+    const elements  = document.getElementsByClassName('ore-grid');
+    const PlanetsInfo  = document.getElementsByClassName('planet-info');
+    const TitelContainer  = document.getElementsByClassName('titel-container');
+    
+    if(window.innerWidth>1700){
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.gridTemplateColumns = 'repeat(4, 1fr)';
+            }
+        var images = document.querySelectorAll('img');
 
-  // Update Agaris circle position
-  agarisCircle.attr({
-    cx: centerX - 300, // Adjust the position
-    cy: centerY - 200 // Adjust the position
-  });
+        // Iterate over all images
+        images.forEach((image) => {
+            // Manipulate each image
+            image.style.width = '400px';
+        });
 
-  // Update The Moon circle position
-  theMoonCircle.attr({
-    cx: centerX - 400, // Adjust the position
-    cy: centerY - 250 // Adjust the position
-  });
+        for (let i = 0; i < PlanetsInfo.length; i++) {
+            PlanetsInfo[i].style.maxWidth = '420px';
+            }
+        for (let i = 0; i < TitelContainer.length; i++) {
+          TitelContainer[i].style.visibility = 'visible';
+          }
 
-  // Update Lezuno circle position
-  lezunoCircle.attr({
-    cx: centerX - 285, // Adjust the position
-    cy: centerY + 200 // Adjust the position
-  });
 
-  // Update Lezuno's Moon circle position
-  lezunoMoonCircle.attr({
-    cx: centerX - 340, // Adjust the position
-    cy: centerY + 250 // Adjust the position
-  });
 
-  // Update Thora4 circle position
-  thora4Circle.attr({
-    cx: centerX + 150, // Adjust the position
-    cy: centerY - 150 // Adjust the position
-  });
+    }
+    else if(window.innerWidth<1700){
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.gridTemplateColumns = 'repeat(2, 1fr)';
+            }
 
-  // Update Crait circle position
-  craitCircle.attr({
-    cx: centerX + 500, // Adjust the position
-    cy: centerY + 100 // Adjust the position
-  });
+        var images = document.querySelectorAll('img');    
+        images.forEach((image) => {
+            // Manipulate each image
+            image.style.width = '200px';
+        });
+        for (let i = 0; i < PlanetsInfo.length; i++) {
+            PlanetsInfo[i].style.maxWidth = '210px';
+            }
+        for (let i = 0; i < TitelContainer.length; i++) {
+          TitelContainer[i].style.visibility = 'hidden';
+          }
+       
+
+            
+        
+    }
+
+
+
+
+    
+    // Update Bylen circle position and outer ring
+    updateCirclePosition(bylenCircle, bylenOuterRing, centerX, centerY);
+
+    // Update Agaris circle position
+    agarisCircle.attr({
+      cx: centerX - 300, // Adjust the position
+      cy: centerY - 200 // Adjust the position
+    });
+
+    // Update The Moon circle position
+    theMoonCircle.attr({
+      cx: centerX - 400, // Adjust the position
+      cy: centerY - 250 // Adjust the position
+    });
+
+    // Update Lezuno circle position
+    lezunoCircle.attr({
+      cx: centerX + 285, // Adjust the position
+      cy: centerY - 200 // Adjust the position
+    });
+
+    // Update Lezuno's Moon circle position
+    lezunoMoonCircle.attr({
+      cx: centerX + 340, // Adjust the position
+      cy: centerY - 250 // Adjust the position
+    });
+
+    // Update Thora4 circle position
+    thora4Circle.attr({
+      cx: centerX - 40, // Adjust the position
+      cy: centerY + 225 // Adjust the position
+    });
+
+    // Update Crait circle position
+    craitCircle.attr({
+      cx: centerX + 500, // Adjust the position
+      cy: centerY + 150 // Adjust the position
+    });
+
+    S27Diamond.attr({
+      cx: centerX + 40, // Adjust the position
+      cy: centerY + 175 // Adjust the position
+    });
+    
 }
 
 // Call the updateCirclePositions function initially and on window resize
